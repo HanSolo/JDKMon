f796909057f7afc58e3d9e854dca6df6ce644f84
Gerrit Grunwald <han.solo@mac.com>
2021-08-18 09:12:53 +0200

    Updated discoclient

--- build_app_linux.sh
+++ build_app_linux.sh
@@ -26,8 +26,8 @@
 # APP_VERSION: the application version, e.g. 1.0.0, shown in "about" dialog
 
 JAVA_VERSION=16
-MAIN_JAR="JDKMon-16.0.11.jar"
-APP_VERSION=16.0.11
+MAIN_JAR="JDKMon-16.0.12.jar"
+APP_VERSION=16.0.12
 
 echo "java home: $JAVA_HOME"
 echo "project version: $PROJECT_VERSION"
