1d047bc2ae6c4c2dc12cb362a2dfaf8120492de4
Gerrit Grunwald <han.solo@mac.com>
2021-11-04 17:24:09 +0100

    new discoclient

--- build_app_macos.sh
+++ build_app_macos.sh
@@ -26,8 +26,8 @@
 # APP_VERSION: the application version, e.g. 1.0.0, shown in "about" dialog
 
 JAVA_VERSION=17
-MAIN_JAR="JDKMon-17.0.10.jar"
-APP_VERSION=17.0.10
+MAIN_JAR="JDKMon-17.0.11.jar"
+APP_VERSION=17.0.11
 
 echo "java home: $JAVA_HOME"
 echo "project version: $PROJECT_VERSION"
