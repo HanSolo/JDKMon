a877f01160cf000d4df0de5d44301c683eb544ee
Gerrit Grunwald <han.solo@mac.com>
2022-07-06 10:28:55 +0200

    Version 17.0.31

--- build_app_macos.sh
+++ build_app_macos.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 JAVA_VERSION=17
-MAIN_JAR="JDKMon-17.0.30.jar"
-APP_VERSION=17.0.30
+MAIN_JAR="JDKMon-17.0.31.jar"
+APP_VERSION=17.0.31
 
 echo "java home: $JAVA_HOME"
 echo "project version: $PROJECT_VERSION"
