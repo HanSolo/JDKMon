1e4aa961f12735e0a4f0bb7c55f35c9fc76752c7
Gerrit Grunwald <han.solo@mac.com>
2022-08-23 08:59:55 +0200

    More cosmetics

--- build_app_windows.bat
+++ build_app_windows.bat
@@ -1,8 +1,8 @@
 @ECHO OFF
 
 set JAVA_VERSION=17
-set MAIN_JAR=JDKMon-17.0.33.jar
-set APP_VERSION=17.0.33
+set MAIN_JAR=JDKMon-17.0.35.jar
+set APP_VERSION=17.0.35
 
 rem ------ SETUP DIRECTORIES AND FILES ----------------------------------------
 rem Remove previously generated java runtime and installers. Copy all required
