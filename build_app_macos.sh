b795b2d107d59756abe8e259c1152827128629b0
Gerrit Grunwald <han.solo@mac.com>
2022-12-05 18:04:03 +0100

    Minor test

--- build_app_macos.sh
+++ build_app_macos.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 JAVA_VERSION=17
-MAIN_JAR="JDKMon-17.0.39.jar"
-APP_VERSION=17.0.39
+MAIN_JAR="JDKMon-17.0.41.jar"
+APP_VERSION=17.0.41
 
 echo "java home: $JAVA_HOME"
 echo "project version: $PROJECT_VERSION"
--- build_app_windows.bat
+++ build_app_windows.bat
@@ -1,8 +1,8 @@
 @ECHO OFF
 
 set JAVA_VERSION=17
-set MAIN_JAR=JDKMon-17.0.39.jar
-set APP_VERSION=17.0.39
+set MAIN_JAR=JDKMon-17.0.41.jar
+set APP_VERSION=17.0.41
 
 rem ------ SETUP DIRECTORIES AND FILES ----------------------------------------
 rem Remove previously generated java runtime and installers. Copy all required
