a7ff6729efbb3a323c29a0e80f6aff3e7e7bfd7a
Gerrit Grunwald <han.solo@mac.com>
2022-12-13 16:31:57 +0100

    Added new version of cvescanner

--- build_app_linux.sh
+++ build_app_linux.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 JAVA_VERSION=17
-MAIN_JAR="JDKMon-17.0.41.jar"
-APP_VERSION=17.0.41
+MAIN_JAR="JDKMon-17.0.43.jar"
+APP_VERSION=17.0.43
 
 echo "java home: $JAVA_HOME"
 echo "project version: $PROJECT_VERSION"
